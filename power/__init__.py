b64929b9160f0e8f9a706877e15596a1cefc0104
anbasile <me@angelobasile.it>
2025-06-02 20:14:09 +0200

    Add a method to estimate power from accuracy.

--- power/__init__.py
+++ power/__init__.py
@@ -4,10 +4,12 @@ from .metrics import (
     find_dataset_size,
     find_minimum_detectable_effect,
 )
+from .utils import make_probability_table
 
 __all__ = [
     "compute_power",
     "classification_report",
     "find_dataset_size",
     "find_minimum_detectable_effect",
+    "make_probability_table",
 ]
